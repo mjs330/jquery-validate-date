0c0340833c48a51e4db6c392b3bba51e18e0fda0
mjs330 <mvs5709@psu.edu>
2016-07-04 09:35:41 -0400

    Refactor

--- jquery-validate-date.js
+++ jquery-validate-date.js
@@ -1,114 +1,32 @@
 //
 // jquery-validate-date
 //
+// Validates a start date and an end date in a set of two dates
+//
+// Example usage:
+// <input name="startDate1" data-start-date="1">
+// <input name="endDate1" data-end-date="1">
+//
 // Dependencies:
 // jQuery - https://jquery.com/
 // jQuery Validation Plugin - https://jqueryvalidation.org/
 //
-
-
-//
 // Options
-//
 var splitter = "/";     //Change date splitter accordingly, example: / or -
 var format = "US";      //"US" for MM/DD/YYYY, "EU" for DD/MM/YYYY
+//
 
 
 $(document).ready(function () {
-    //
-    //Check past/future dates
-    //
-    $('input[data-date-past]').blur(function () {
-        var now = new Date();
-        var date = toDate($(this).val());
-        if (date > now) {
-            var name = $(this).attr('name');
-            eval("$validator.showErrors({ " + name + ": \"Please enter a past date.\" });");
-        }
-    });
-    $('input[data-date-future]').blur(function () {
-        var now = new Date();
-        var date = toDate($(this).val());
-        if (date < now) {
-            var name = $(this).attr('name');
-            eval("$validator.showErrors({ " + name + ": \"Please enter a future date.\" });");
-        }
-    });
-    $("input[data-date-past]").datepicker('change', { minDate: -99999, maxDate: 0 });
-    $("input[data-date-future]").datepicker('change', { minDate: 0, maxDate: 99999 });
-    //
-    //End Check past/future dates
-    //
-
-
-    //
-    //Check start/end dates
-    //
-    $('input[data-date-start]').change(function () {
-        var dateStart = toDate($(this).val());
-        var dateEnd = dateStart;
-        var linker = $(this).attr('data-date-start');
-        $('input[data-date-end]').each(function () {
-            if ($(this).attr('data-date-end') == linker) {
-                dateEnd = toDate($(this).val());
-                $(this).datepicker('change', {
-                    minDate: dateStart
-                });
-            }
-        });
-    });
-    $('input[data-date-start]').blur(function () {
-        var dateStart = toDate($(this).val());
-        var dateEnd = dateStart;
-        var linker = $(this).attr('data-date-start');
-        $('input[data-date-end]').each(function () {
-            if ($(this).attr('data-date-end') == linker) {
-                dateEnd = toDate($(this).val());
-            }
-        });
-        if (dateStart > dateEnd) {
-            var name = $(this).attr('name');
-            eval("$validator.showErrors({ " + name + ": \"Please enter a valid start date.\" });");
-        }
-    });
-    $('input[data-date-end]').change(function () {
-        var dateEnd = toDate($(this).val());
-        var dateStart = dateEnd;
-        var linker = $(this).attr('data-date-end');
-        $('input[data-date-start]').each(function () {
-            if ($(this).attr('data-date-start') == linker) {
-                dateStart = toDate($(this).val());
-                $(this).datepicker('change', {
-                    maxDate: dateEnd
-                });
-            }
-        });
-    });
-    $('input[data-date-end]').blur(function () {
-        var dateEnd = toDate($(this).val());
-        var dateStart = dateEnd;
-        var linker = $(this).attr('data-date-end');
-        $('input[data-date-start]').each(function () {
-            if ($(this).attr('data-date-start') == linker) {
-                dateStart = toDate($(this).val());
-            }
-        });
-        if (dateStart > dateEnd) {
-            var name = $(this).attr('name');
-            eval("$validator.showErrors({ " + name + ": \"Please enter a valid end date.\" });");
-        }
-    });
-    $("input[data-date-start]").datepicker();
-    $("input[data-date-end]").datepicker();
-
-    
+    //Start/End date validation
     jQuery.validator.addMethod("ranged", function (value, element) {
         var dateEnd = toDate(value);
         var dateStart = dateEnd;
+        var linked = null;
         //Check for end date attr
         var linker = element.getAttribute('data-end-date');
         if (linker == null) {
-            //Check for start end attr
+            //Check for start date attr
             linker = element.getAttribute('data-start-date');
             if (linker == null) {
                 return true;
@@ -117,11 +35,15 @@ $(document).ready(function () {
                 $('input[data-end-date]').each(function () {
                     if ($(this).attr('data-end-date') == linker) {
                         dateEnd = toDate($(this).val());
+                        linked = this;
                     }
                 });
                 if (dateStart > dateEnd) {
                     return false;
                 } else {
+                    if ($(linked).hasClass('error')) {
+                        $(linked).removeClass('error')
+                    }
                     return true;
                 }
             }
@@ -130,30 +52,63 @@ $(document).ready(function () {
             $('input[data-start-date]').each(function () {
                 if ($(this).attr('data-start-date') == linker) {
                     dateStart = toDate($(this).val());
+                    linked = this;
                 }
             });
             if (dateStart > dateEnd) {
                 return false;
             } else {
+                if ($(linked).hasClass('error')) {
+                    $(linked).removeClass('error')
+                }
                 return true;
             }
         }
     }, "Please enter a valid date range.");
-    $( 'input[data-date-start]' ).rules( 'add', {
-        ranged: true,
+    jQuery.validator.addClassRules({
+        dateSelect: {
+            ranged: true
+        },
+        date: {
+            ranged: true
+        }
     });
-    $( 'input[data-date-end]' ).rules( 'add', {
-        ranged: true,
-    });
-    //
-    //End Check start/end dates
-    //
+    //Validate when date range is changed
+    $('body').on('change blur', 'input[data-start-date]', function () {
+        var dateEnd = "Invalid Date";
+        var linker = $(this).attr('data-start-date');
+        $('input[data-end-date]').each(function () {
+            if ($(this).attr('data-end-date') == linker) {
+                dateEnd = toDate($(this).val());
+                if (dateEnd != "Invalid Date") {
+                    $(this).valid();
+                }
 
+            }
+        });
+        if (dateEnd != "Invalid Date") {
+            $(this).valid();
+        }
+    });
+    $('body').on('change blur', 'input[data-end-date]', function () {
+        var dateStart = "Invalid Date";
+        var linker = $(this).attr('data-end-date');
+        $('input[data-start-date]').each(function () {
+            if ($(this).attr('data-start-date') == linker) {
+                dateStart = toDate($(this).val());
+                if (dateStart != "Invalid Date") {
+                    $(this).valid();
+                }
+
+            }
+        });
+        if (dateStart != "Invalid Date") {
+            $(this).valid();
+        }
+    });
 });
 
-//
 //Convert date
-//
 function toDate(dateStr) {
     var parts = dateStr.split(splitter);
     if (format = "US") {
@@ -164,4 +119,3 @@ function toDate(dateStr) {
         return null;
     }
 }
-
