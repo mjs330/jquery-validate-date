8b4cf46979b4c0ca0460f7c4f3aa044971ead307
Marc <mjs330@users.noreply.github.com>
2018-03-20 11:41:40 -0400

    Temporarily remove current, past, future validation

--- jquery.validate.date.js
+++ jquery.validate.date.js
@@ -1,5 +1,6 @@
 //
 // jquery.validate.date
+// https://github.com/mjs330
 //
 // Validates a start date and an end date in a set of two dates
 // Validates that a date is in the past or future
@@ -25,23 +26,19 @@
 
 // jvdate object
 var jvdate = {
-    dateformat: "M"
+    dateformat: "M",
+    onchange: true
 };
-
-jvdate.initvalidator = function() {
-        $.validate();
-        this.bind();
-}
 
 jvdate.format = function (format) {
     if (format == null || format == "") {
         return;
-    } else if (format == "M" || format == "L" || format == "B") { 
+    } else if (format == "M" || format == "L" || format == "B") {
         jvdate.dateformat = format;
     } else if (format.indexOf("MM") > -1 && format.indexOf("DD") > -1 && format.indexOf("YYYY") > -1
         && (format.indexOf("/") > -1 || format.indexOf("-") > -1 || format.indexOf("*") > -1)) {
         var dateSplitter = jvdate.findsplitter(format);
-        switch(format) {
+        switch (format) {
             case "MM" + dateSplitter + "DD" + dateSplitter + "YYYY":
                 jvdate.dateformat = "M";
                 break;
@@ -50,7 +47,7 @@ jvdate.format = function (format) {
                 break;
             case "YYYY" + dateSplitter + "MM" + dateSplitter + "DD":
                 jvdate.dateformat = "B";
-                break; 
+                break;
         }
     }
 };
@@ -59,18 +56,27 @@ jvdate.setformat = function (format) {
     jvdate.format(format);
 };
 
-jvdate.todate = function(dateStr) {
+jvdate.setonchange = function (toggle) {
+    if (toggle != true && toggle != false) {
+        return;
+    } else {
+        onchange = true;
+        jvdate.bind;
+    }
+}
+
+jvdate.todate = function (dateStr) {
     var dateSplitter = jvdate.findsplitter(dateStr);
     if (dateSplitter == "") {
         return "Invalid Date";
     }
     var parts = dateStr.split(dateSplitter);
     if (jvdate.dateformat = "M") {
-        return (new Date(parts[2], parts[0] - 1, parts[1])).setHours(0,0,0,0);
+        return (new Date(parts[2], parts[0] - 1, parts[1])).setHours(0, 0, 0, 0);
     } else if (jvdate.dateformat = "L") {
-        return (new Date(parts[2], parts[1] - 1, parts[0])).setHours(0,0,0,0);
+        return (new Date(parts[2], parts[1] - 1, parts[0])).setHours(0, 0, 0, 0);
     } else if (jvdate.dateformat = "B") {
-        return (new Date(parts[0], parts[1] - 1, parts[2])).setHours(0,0,0,0);
+        return (new Date(parts[0], parts[1] - 1, parts[2])).setHours(0, 0, 0, 0);
     } else {
         return "Invalid Date";
     }
@@ -90,7 +96,7 @@ jvdate.findsplitter = function (dateStr) {
     }
 };
 
-jvdate.rebind = function() {
+jvdate.rebind = function () {
     this.bind();
 };
 
@@ -110,7 +116,7 @@ jvdate.bind = function () {
             } else {
                 //Handle start date attr
                 $('input[data-end-date]').each(function () {
-                    if ($(this).attr('data-end-date') == linker) {
+                    if ($(this).data('end-date') == linker) {
                         dateEnd = jvdate.todate($(this).val());
                         linked = this;
                     }
@@ -127,7 +133,7 @@ jvdate.bind = function () {
         } else {
             //Handle end date attr
             $('input[data-start-date]').each(function () {
-                if ($(this).attr('data-start-date') == linker) {
+                if ($(this).data('start-date') == linker) {
                     dateStart = jvdate.todate($(this).val());
                     linked = this;
                 }
@@ -153,102 +159,58 @@ jvdate.bind = function () {
         });
     });
 
-    //Validate start/end date when date range is changed
-    $('body').on('change keyup', 'input[data-start-date]', function () {
-        var dateEnd = "Invalid Date";
-        var linker = $(this).attr('data-start-date');
-        $('input[data-end-date]').each(function () {
-            if ($(this).attr('data-end-date') == linker) {
-                dateEnd = jvdate.todate($(this).val());
-                if (dateEnd != "Invalid Date") {
-                    $(this).valid();
-                }
-            }
-        });
-        if (dateEnd != "Invalid Date") {
-            $(this).valid();
-        }
-    });
-    $('body').on('change keyup', 'input[data-end-date]', function () {
-        var dateStart = "Invalid Date";
-        var linker = $(this).attr('data-end-date');
-        $('input[data-start-date]').each(function () {
-            if ($(this).attr('data-start-date') == linker) {
-                dateStart = jvdate.todate($(this).val());
-                if (dateStart != "Invalid Date") {
-                    $(this).valid();
-                }
-            }
-        });
-        if (dateStart != "Invalid Date") {
-            $(this).valid();
-        }
-    });
+    ////Validate current date
+    //jQuery.validator.addMethod("isCurrent", function (value, element) {
+    //    if (typeof ($(element).data('past-date') != 'undefined') || typeof ($(element).data('future-date') != 'undefined')) {
+    //        return true;
+    //    } else {
+    //        var date = (jvdate.todate($(element).val()));
+    //        var now = (new Date()).setHours(0, 0, 0, 0);
+    //        if (date == now) {
+    //            return true;
+    //        } else {
+    //            return false;
+    //        }
+    //    }
 
-    //Validate current date
-    jQuery.validator.addMethod("isCurrent", function (value, element) {
-        if (($(element).attr('data-past-date') != undefined) || ($(element).attr('data-future-date') != undefined)) {
-            return true;
-        } else {
-            var date = (jvdate.todate($(element).val()));
-            var now = (new Date());
-            if (date == now) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+    //}, "Please enter a valid current date.");
+    //$("input[data-current-date]").each(function () {
+    //    $(this).rules("add", {
+    //        isCurrent: true
+    //    });
+    //});
 
-    }, "Please enter a valid current date.");
-    $("input[data-current-date]").each(function () {
-        $(this).rules("add", {
-            isCurrent: true
-        });
-    });
+    ////Validate past date
+    //jQuery.validator.addMethod("inPast", function (value, element) {
+    //    var now = (new Date()).setHours(0, 0, 0, 0);
+    //    var date = (jvdate.todate($(element).val()));
+    //    if (((date == now) && typeof ($(element).data('current-date') != undefined)) || (date < now)) {
+    //        return true;
+    //    } else {
+    //        return false;
+    //    }
+    //}, "Please enter a valid past date.");
+    //$("input[data-past-date]").each(function () {
+    //    $(this).rules("add", {
+    //        inPast: true
+    //    });
+    //});
 
-    //Validate past date
-    jQuery.validator.addMethod("inPast", function (value, element) {
-        var now = (new Date());
-        var date = (jvdate.todate($(element).val()));
-        if (((date == now) && ($(element).attr('data-current-date') != undefined)) || (date < now)) {
-            return true;
-        } else {
-            return false;
-        }
-    }, "Please enter a valid past date.");
-    $("input[data-past-date]").each(function () {
-        $(this).rules("add", {
-            inPast: true
-        });
-    });
-
-    //Validate future date
-    jQuery.validator.addMethod("inFuture", function (value, element) {
-        var now = (new Date());
-        var date = (jvdate.todate($(element).val()));
-        if (((date == now) && ($(element).attr('data-current-date') != undefined)) || (date > now)) {
-            return true;
-        } else {
-            return false;
-        }
-    }, "Please enter a valid future date.");
-    $("input[data-future-date]").each(function () {
-        $(this).rules("add", {
-            inFuture: true
-        });
-    });
-
-    //Validate past/future date when changed
-    $('body').on('change keyup', 'input[data-future-date]', function () {
-        if (jvdate.todate($(this).val()) != "Invalid Date") {
-            $(this).valid();
-        }
-    });
-    $('body').on('change keyup', 'input[data-end-date]', function () {
-        if (jvdate.todate($(this).val()) != "Invalid Date") {
-            $(this).valid();
-        }
-    });
+    ////Validate future date
+    //jQuery.validator.addMethod("inFuture", function (value, element) {
+    //    var now = (new Date()).setHours(0, 0, 0, 0);
+    //    var date = (jvdate.todate($(element).val()));
+    //    if (((date == now) && typeof ($(element).data('current-date') != undefined)) || (date > now)) {
+    //        return true;
+    //    } else {
+    //        return false;
+    //    }
+    //}, "Please enter a valid future date.");
+    //$("input[data-future-date]").each(function () {
+    //    $(this).rules("add", {
+    //        inFuture: true
+    //    });
+    //});
 
     //Validate max date
     jQuery.validator.addMethod("beforeOrAtMax", function (value, element) {
@@ -291,4 +253,62 @@ jvdate.bind = function () {
             afterOrAtMin: true
         });
     });
+
+
+    //Validate on change
+    if (jvdate.onchange == true) {
+        ////Validate past/future date when changed
+        //$('body').on('change keyup', 'input[data-future-date]', function () {
+        //    if (jvdate.todate($(this).val()) != "Invalid Date") {
+        //        $(this).valid();
+        //    }
+        //});
+        //$('body').on('change keyup', 'input[data-past-date]', function () {
+        //    if (jvdate.todate($(this).val()) != "Invalid Date") {
+        //        $(this).valid();
+        //    }
+        //});
+
+        //Validate start/end date when date range is changed
+        $('body').on('change keyup', 'input[data-start-date]', function () {
+            var dateEnd = "Invalid Date";
+            var linker = $(this).data('start-date');
+            $('input[data-end-date]').each(function () {
+                if ($(this).data('end-date') == linker) {
+                    dateEnd = jvdate.todate($(this).val());
+                    if (dateEnd != "Invalid Date") {
+                        $(this).valid();
+                    }
+                }
+            });
+            if (dateEnd != "Invalid Date") {
+                $(this).valid();
+            }
+        });
+        $('body').on('change keyup', 'input[data-end-date]', function () {
+            var dateStart = "Invalid Date";
+            var linker = $(this).data('end-date');
+            $('input[data-start-date]').each(function () {
+                if ($(this).data('start-date') == linker) {
+                    dateStart = jvdate.todate($(this).val());
+                    if (dateStart != "Invalid Date") {
+                        $(this).valid();
+                    }
+                }
+            });
+            if (dateStart != "Invalid Date") {
+                $(this).valid();
+            }
+        });
+    } else {
+        $('body').on('change keyup', 'input[data-future-date]', function () {
+        });
+        $('body').on('change keyup', 'input[data-end-date]', function () {
+        });
+        $('body').on('change keyup', 'input[data-start-date]', function () {
+        });
+        $('body').on('change keyup', 'input[data-end-date]', function () {
+        });
+    }
+
 };
