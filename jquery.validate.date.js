e5ff4d29a09f91d27ade4c4aba153dd54a5463c6
Marc <mjs330@users.noreply.github.com>
2018-05-21 13:50:04 -0400

    Add MIT License

--- jquery.validate.date.js
+++ jquery.validate.date.js
@@ -7,7 +7,7 @@
 // jQuery Validation Plugin - https://jqueryvalidation.org/
 //
 // MIT License
-// Copyright (c) 2016 Marc Sozynski
+// Copyright (c) 2016-2018 Marc Sozynski
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
